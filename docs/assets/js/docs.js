50937fdea8ff54497920008ced7e162e5158ec38
Andrews Ang <kongnir@gmail.com>
2015-08-14 17:22:50 +0800

    Bug fix #8: Resolved Circle button icon not aligned in the center. New #7: Corner ribbons with multiple colors. Improved: Pre code removed border to fit in the flat design.

--- docs/assets/js/docs.js
+++ docs/assets/js/docs.js
@@ -7,91 +7,130 @@
     app.directive("viewBanner", function () {
         return {
             restrict: "E",
-            templateUrl: "views/banner.html"
+            templateUrl: "views/banner.html",
+            link: {
+                post: sharedLinkFunction
+            }
         };
     });
     
     app.directive("viewNavbar", function () {
         return {
             restrict: "E",
-            templateUrl: "views/navbar.html"
+            templateUrl: "views/navbar.html",
+            link: {
+                post: sharedLinkFunction
+            }
         };
     });
     
     app.directive("viewButtons", function () {
         return {
             restrict: "E",
-            templateUrl: "views/buttons.html"
+            templateUrl: "views/buttons.html",
+            link: {
+                post: sharedLinkFunction
+            }
         };
     });
     
     app.directive("viewTypography", function () {
         return {
             restrict: "E",
-            templateUrl: "views/typography.html"
+            templateUrl: "views/typography.html",
+            link: {
+                post: sharedLinkFunction
+            }
         };
     });
     
     app.directive("viewTables", function () {
         return {
             restrict: "E",
-            templateUrl: "views/tables.html"
+            templateUrl: "views/tables.html",
+            link: {
+                post: sharedLinkFunction
+            }
         };
     });
     
     app.directive("viewForms", function () {
         return {
             restrict: "E",
-            templateUrl: "views/forms.html"
+            templateUrl: "views/forms.html",
+            link: {
+                post: sharedLinkFunction
+            }
         };
     });
     
     app.directive("viewNavs", function () {
         return {
             restrict: "E",
-            templateUrl: "views/navs.html"
+            templateUrl: "views/navs.html",
+            link: {
+                post: sharedLinkFunction
+            }
         };
     });
     
     app.directive("viewIndicators", function () {
         return {
             restrict: "E",
-            templateUrl: "views/indicators.html"
+            templateUrl: "views/indicators.html",
+            link: {
+                post: sharedLinkFunction
+            }
         };
     });
     
     app.directive("viewProgressbars", function () {
         return {
             restrict: "E",
-            templateUrl: "views/progressbars.html"
+            templateUrl: "views/progressbars.html",
+            link: {
+                post: sharedLinkFunction
+            }
         };
     });
     
     app.directive("viewContainers", function () {
         return {
             restrict: "E",
-            templateUrl: "views/containers.html"
+            templateUrl: "views/containers.html",
+            link: {
+                post: sharedLinkFunction
+            }
         };
     });
   
     app.directive("viewPanels", function () {
         return {
             restrict: "E",
-            templateUrl: "views/panels.html"
+            templateUrl: "views/panels.html",
+            link: {
+                post: sharedLinkFunction
+            }
         };
     });
     
     app.directive("viewDialogs", function () {
         return {
             restrict: "E",
-            templateUrl: "views/dialogs.html"
+            templateUrl: "views/dialogs.html",
+            link: {
+                post: sharedLinkFunction
+            }
         };
     });
     
     app.directive("viewFooter", function () {
         return {
             restrict: "E",
-            templateUrl: "views/footer.html"
+            templateUrl: "views/footer.html",
+            link: {
+                post: sharedLinkFunction
+            }
         };
     });
     
@@ -99,23 +138,52 @@
     app.directive("viewRedmaterialsButtons", function () {
         return {
             restrict: "E",
-            templateUrl: "views/redmaterials/buttons.html"
+            templateUrl: "views/redmaterials/buttons.html",
+            link: {
+                post: sharedLinkFunction
+            }
         };
     });
   
     app.directive("viewRedmaterialsTables", function () {
         return {
             restrict: "E",
-            templateUrl: "views/redmaterials/tables.html"
+            templateUrl: "views/redmaterials/tables.html",
+            link: {
+                post: sharedLinkFunction
+            }
         };
     });
     
     app.directive("viewRedmaterialsShadow", function () {
         return {
             restrict: "E",
-            templateUrl: "views/redmaterials/shadows.html"
+            templateUrl: "views/redmaterials/shadows.html",
+            link: {
+                post: sharedLinkFunction
+            }
         };
     });
+    
+    app.directive("viewRedmaterialsRibbons", function () {
+        return {
+            restrict: "E",
+            templateUrl: "views/redmaterials/ribbons.html",
+            link: {
+                post: sharedLinkFunction
+            }
+        };
+    });
+    
+    // Scroll to anchor
+    function sharedLinkFunction( scope, element, attributes ) {
+        var hash = window.location.hash.substr(1);
+        var $hashTag = $('#' + hash);
+        if ($hashTag.length > 0) {
+            $('html, body').animate({scrollTop: $hashTag.offset().top}, 0);
+        }
+    }
+    
 }());
 
 (function ($) {
