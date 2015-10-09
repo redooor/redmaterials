48bdea81090af93c2fc30aac4b845c84b564dbee
Andrews Ang <kongnir@gmail.com>
2015-10-09 15:14:13 +0800

    Added Syntax Highlighting to documentations.

--- docs/assets/js/docs.js
+++ docs/assets/js/docs.js
@@ -194,6 +194,18 @@
         }
     }
     
+    // Prism for syntax highlighting
+    app.directive('ngPrism', [function() {
+        return {
+            restrict: 'A',
+            link: function($scope, element, attrs) {
+                element.ready(function() {
+                    Prism.highlightElement(element[0]);
+                });
+            }
+        }
+    }]);
+    
 }());
 
 (function ($) {
