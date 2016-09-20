71bdb70aa0e49587743cde43cbad6af34e00059b
Andrews Ang <kongnir@gmail.com>
2016-09-20 17:24:02 +0800

    Doc: added instructions for new Nav Sidebar.

--- docs/assets/js/docs.js
+++ docs/assets/js/docs.js
@@ -1,8 +1,17 @@
 /*jslint browser: true, plusplus: true, vars: true*/
-/*global $, jQuery, angular*/
+/*global $, jQuery, angular, Prism*/
 (function () {
     'use strict';
     var app = angular.module("docs", []);
+    
+    // Scroll to anchor on load
+    function sharedLinkFunction(scope, element, attributes) {
+        var hash = window.location.hash.substr(1);
+        var $hashTag = $('#' + hash);
+        if ($hashTag.length > 0) {
+            $('html, body').animate({scrollTop: $hashTag.offset().top}, 0);
+        }
+    }
     
     app.directive("viewBanner", function () {
         return {
@@ -205,27 +214,27 @@
         };
     });
     
-    // Scroll to anchor
-    function sharedLinkFunction( scope, element, attributes ) {
-        var hash = window.location.hash.substr(1);
-        var $hashTag = $('#' + hash);
-        if ($hashTag.length > 0) {
-            $('html, body').animate({scrollTop: $hashTag.offset().top}, 0);
-        }
-    }
+    app.directive("viewRedmaterialsNavSidebar", function () {
+        return {
+            restrict: "E",
+            templateUrl: "views/redmaterials/nav-sidebar.html",
+            link: {
+                post: sharedLinkFunction
+            }
+        };
+    });
     
     // Prism for syntax highlighting
-    app.directive('ngPrism', [function() {
+    app.directive('ngPrism', [function () {
         return {
             restrict: 'A',
-            link: function($scope, element, attrs) {
-                element.ready(function() {
+            link: function ($scope, element, attrs) {
+                element.ready(function () {
                     Prism.highlightElement(element[0]);
                 });
             }
-        }
+        };
     }]);
-    
 }());
 
 (function ($) {
@@ -235,12 +244,20 @@
         $('[data-toggle="popover"]').popover();
         function setActiveMenu() {
             var anchor = window.location.hash;
-            $('#nav_sidebar1 .nav li a').each(function() {
-                if ($(this).attr('href') == anchor) {
+            $('#nav_sidebar1 .nav li a').each(function () {
+                if ($(this).attr('href') === anchor) {
                     $(this).parent().addClass('active');
                 }
             });
         }
         setActiveMenu();
+        $(document).on('click touchstart', '#nav_sidebar1 .nav li a', function () {
+            var goto, hashTag;
+            goto = $(this).attr('href').substr(1);
+            hashTag = $('#' + goto);
+            if (hashTag.length > 0) {
+                $('html, body').animate({scrollTop: hashTag.offset().top}, 300);
+            }
+        });
     });
 }(window.jQuery));
